ddf4e31b2f095cb608c8b3501d5c69944bdaa306
Shigatsi <shigatsii@gmail.com>
2021-12-26 23:38:32 +0300

    feat: styles added

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -17,48 +17,52 @@ function Converter({
       {isLoading ? (
         <Spinner />
       ) : (
-        <div className="converter__input-container">
-          <input
-            id="base"
-            className="converter__input"
-            type="text"
-            value={baseCurrensyVal}
-            maxLength="12"
-            onKeyUp={(evt) => inputHandler(evt, "base")}
-            onChange={(evt) => inputHandler(evt, "baseVal")}
-          ></input>
-          <select
-            id="selectBase"
-            className="converter__selector"
-            onChange={(evt) => selectedCurrensyHandler(evt, "base")}
-          >
-            {availebleCurrensies.map((item, i) => (
-              <option selected={baseCurrensy === item ? true : false} key={i}>
-                {item}
-              </option>
-            ))}
-          </select>
-          <input
-            id="convert"
-            className="converter__result"
-            value={convertedCurrensyVal}
-            onKeyUp={(evt) => inputHandler(evt, "converted")}
-            onChange={(evt) => inputHandler(evt, "convertedVal")}
-          ></input>
-          <select
-            id="selectConverted"
-            className="converter__selector"
-            onChange={(evt) => selectedCurrensyHandler(evt, "converted")}
-          >
-            {availebleCurrensies.map((item, i) => (
-              <option
-                selected={convertedCurrensy === item ? true : false}
-                key={i}
-              >
-                {item}
-              </option>
-            ))}
-          </select>
+        <div className="converter__container">
+          <div className="converter__container-input-select">
+            <input
+              id="base"
+              className="converter__input"
+              type="text"
+              value={baseCurrensyVal}
+              maxLength="12"
+              onKeyUp={(evt) => inputHandler(evt, "base")}
+              onChange={(evt) => inputHandler(evt, "baseVal")}
+            ></input>
+            <select
+              id="selectBase"
+              className="converter__selector"
+              onChange={(evt) => selectedCurrensyHandler(evt, "base")}
+            >
+              {availebleCurrensies.map((item, i) => (
+                <option selected={baseCurrensy === item ? true : false} key={i}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="converter__container-input-select">
+            <input
+              id="convert"
+              className="converter__result"
+              value={convertedCurrensyVal}
+              onKeyUp={(evt) => inputHandler(evt, "converted")}
+              onChange={(evt) => inputHandler(evt, "convertedVal")}
+            ></input>
+            <select
+              id="selectConverted"
+              className="converter__selector"
+              onChange={(evt) => selectedCurrensyHandler(evt, "converted")}
+            >
+              {availebleCurrensies.map((item, i) => (
+                <option
+                  selected={convertedCurrensy === item ? true : false}
+                  key={i}
+                >
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       )}
     </div>
